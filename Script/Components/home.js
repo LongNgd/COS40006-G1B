cd2df9faa54bd75f509778200b938709f9ea4046
LongNgd <nguyendinhlong2112@gmail.com>
2024-08-19 21:02:21 +0700

    Update home page - upload section

--- Script/Components/home.js
+++ Script/Components/home.js
@@ -4,7 +4,7 @@ const home = {
         <div class="bg-white rounded-3 px-5 py-3">
             <h2>Upload section</h2>
             <p>Our application is a powerful tool designed to transform CCTV data into insights. By analyzing footage for anomalies, the tool generates visualization like heatmaps and charts and offers a report exporter function to create detailed reports.</p>
-            <div></div>
+            <div id="browse-file" class="bg-secondary my-4 mx-auto"></div>
         </div>
     </div>
     `
