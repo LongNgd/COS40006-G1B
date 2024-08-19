1fe1c1d37f6a7f680ce5e544a43312b5c89b8c78
LongNgd <nguyendinhlong2112@gmail.com>
2024-08-19 19:43:19 +0700

    Update navigation bar and home page

--- Script/Components/home.js
+++ Script/Components/home.js
@@ -1,5 +1,11 @@
 const home = {
     template: `
-    <h1>Home</h1>
+    <div id="home" class="d-flex justify-content-center align-items-center">
+        <div class="bg-white rounded-3 px-5 py-3">
+            <h2>Upload section</h2>
+            <p>Our application is a powerful tool designed to transform CCTV data into insights. By analyzing footage for anomalies, the tool generates visualization like heatmaps and charts and offers a report exporter function to create detailed reports.</p>
+            <div></div>
+        </div>
+    </div>
     `
 }
--- Script/Components/nav.js
+++ Script/Components/nav.js
@@ -1,17 +1,22 @@
 const nav_template = `
-    <ul class="nav justify-content-end">
-        <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
+    <ul class="nav justify-content-between align-items-center bg-white border-bottom">
+        <li class="nav-item mx-3">
+            <img src="./Style/Images/FPT-logo.png" alt="Logo" class="nav-logo">
         </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/project">Projects</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/login">Login</router-link>
-        </li>
-        <li class="nav-item">
-            <router-link class="nav-link" to="/register">Register</router-link>
-        </li>
+        <div class = "d-flex">
+            <li class="nav-item">
+                <router-link class="nav-link align-center" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link align-center" to="/project">Projects</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link align-center pe-0" to="/login"><button type="button" class="btn btn-primary">Login</button></router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link align-center me-3" to="/register"><button type="button" class="btn btn-secondary">Register</button></router-link>
+            </li>
+        </div>
     </ul>
 `;
 
