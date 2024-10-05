373475fda5a736a192015f5059b9bf0d169c0f4a
QDuy <nguyenquocduy0207@gmail.com>
2024-10-05 22:35:02 +0700

    refactor: Update import paths for Anomaly type

--- frontend/src/components/Pages/About.tsx
+++ frontend/src/components/Pages/About.tsx
@@ -1,6 +1,38 @@
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { aboutContents } from './about.info'
+
 const About = () => {
   return (
-    <div>About</div>
+    <div className="m-4">
+      {aboutContents.map((content) => {
+        return (
+          <>
+            <div className="flex justify-between pb-2 text-xl">
+              {content.title}
+            </div>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-5">
+              {content.items.map((item) => {
+                return (
+                  <Card>
+                    <CardHeader className="flex flex-row gap-2 items-center justify-center p-4">
+                      <img src={item.image} alt={item.title} width={50} />
+                      <CardTitle className="text-xl font-medium">
+                        {item.title}
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent className="p-3 pt-0">
+                      <p className="text-xs text-muted-foreground">
+                        {item.description}
+                      </p>
+                    </CardContent>
+                  </Card>
+                )
+              })}
+            </div>
+          </>
+        )
+      })}
+    </div>
   )
 }
 
---
+++ frontend/src/components/Pages/about.info.tsx
@@ -0,0 +1,63 @@
+export const aboutContents = [
+    {
+        title: 'Frontend',
+        items: [
+            {
+                title: 'React',
+                description: 'A JavaScript library for building user interfaces',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png'
+            },
+            {
+                title: 'Type Script',
+                description: 'The frontend is written in TypeScript, providing type safety and improved developer experience.',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png'
+            },
+            {
+                title: 'Vite',
+                description: 'Vite is used as the build tool, which is known for its fast development server and optimized build process.',
+                image: 'https://seeklogo.com/images/V/vite-logo-BFD4283991-seeklogo.com.png'
+            },
+            {
+                title: 'Tailwind CSS',
+                description: 'A utility-first CSS framework for rapidly building custom designs',
+                image: 'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,h_1080,q_100,w_1080/v1/gcs/platform-data-dsc/events/Tailwind_CSS_Logo.svg_GkNDLAs.png'
+            },
+            {
+                title: 'Shadcn UI',
+                description: 'A collection of reusable React components for building great user interfaces',
+                image: 'https://suhelmakkad.gallerycdn.vsassets.io/extensions/suhelmakkad/shadcn-ui/0.1.1/1695843310552/Microsoft.VisualStudio.Services.Icons.Default'
+            },
+            {
+                title: 'Ant Design',
+                description: 'A design system for enterprise-level products. Create an efficient and enjoyable work experience',
+                image: 'https://th.bing.com/th/id/OIP._ArbpXVAHHuiMsb2743MagHaHa?rs=1&pid=ImgDetMain'
+            },
+            {
+                title: 'Redux Toolkit',
+                description: 'The official, opinionated, batteries-included toolset for efficient Redux development',
+                image: 'https://redux-toolkit.js.org/img/redux.svg'
+            },
+            
+        ]
+    },
+    {
+        title: 'Backend',
+        items: [
+            {
+                title: 'Flask',
+                description: 'The backend is built using Flask, a lightweight WSGI web application framework in Python.',
+                image: 'https://kimsereylam.com/assets/logos/flask.png'
+            },
+            {
+                title: 'PostgreSQL',
+                description: 'PostgreSQL is used as the database to store the application data.',
+                image: 'https://icon-library.com/images/postgres-icon/postgres-icon-7.jpg'
+            },
+            {
+                title: 'Swagger',
+                description: 'Swagger is used to document the API endpoints and provide an interactive API explorer.',
+                image: 'https://static-00.iconduck.com/assets.00/swagger-icon-2048x2048-563qbzey.png'
+            }
+        ]
+    }
+]
